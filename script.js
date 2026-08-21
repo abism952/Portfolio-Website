217cc956c2dbcdd13b7f2ec2945c9621536319aa
Muhammad Abis <abism952@gmail.com>
2026-08-21 08:01:17 -0700

    update files

--- script.js
+++ script.js
@@ -18,60 +18,69 @@ let charIndex = 0;
 
 const typing = document.querySelector(".typing");
 
-function type(){
+function type() {
 
-  if(charIndex < text[index].length){
+  if (charIndex < text[index].length) {
 
     typing.textContent += text[index].charAt(charIndex);
 
     charIndex++;
 
-    setTimeout(type,100);
+    setTimeout(type, 100);
 
   }
-  else{
+  else {
 
-    setTimeout(erase,1500);
+    setTimeout(erase, 1500);
 
   }
 
 }
 
-function erase(){
+function erase() {
 
-  if(charIndex > 0){
+  if (charIndex > 0) {
 
-    typing.textContent = text[index].substring(0,charIndex-1);
+    typing.textContent = text[index].substring(0, charIndex - 1);
 
     charIndex--;
 
-    setTimeout(erase,50);
+    setTimeout(erase, 50);
 
   }
-  else{
+  else {
 
     index++;
 
-    if(index >= text.length){
+    if (index >= text.length) {
       index = 0;
     }
 
-    setTimeout(type,500);
+    setTimeout(type, 500);
 
   }
 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(type,500);
+  setTimeout(type, 500);
 });
 
-function  clk(){
-     window.open("cv.pdf.pdf", "_blank");
+
+// ================= CV =================
+function clk() {
+  window.open("cv.pdf.pdf", "_blank");
 };
 
-function changebg (){
-  var abc = document.getElementById ("clr").value
-  var bg = document.getElementById("color")
-  document.body.style.background=abc;
+// ================= WEB BACKGROUND  =================
+function changebg() {
+  var abc = document.getElementById("clr").value;
+
+  document.body.style.backgroundColor = abc;
+}
+// ================= FORM =================
+function btn2(event) {
+  event.preventDefault();
+
+  alert("Data Successfully Submitted");
 }
